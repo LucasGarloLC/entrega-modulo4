59d1da6ac93d994215b0e9caf4762f5885b94901
Lucas <lgltrad@gmail.com>
2023-05-11 16:32:21 +0200

    Version 1.0.

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,43 @@
-/*
 
-- titulo
-- director
-- año
+// Defines the constants for the task
+const addBtn = (document.getElementById("añadir") as HTMLInputElement)
+const substractBtn = (document.getElementById("restar") as HTMLInputElement);
+const resetBtn = (document.getElementById("reiniciar") as HTMLInputElement);
+const btn1 = (document.getElementById("number") as HTMLInputElement);
+const sendNumber = (document.getElementById("btn1") as HTMLButtonElement);
+const pCount = (document.getElementById("count") as HTMLInputElement);
 
-Blade Runner / Ridley Scoot / 1982
-Matrix / Lana Wachowski / 1989
+let turno = 0;
 
-*/
+// Adds a number
+function add(){
+  if (addBtn !==null && addBtn !== undefined)
+  turno++
+  pCount.innerHTML = turno.toString().padStart(2, "0");
+}
 
-interface Pelicula {
-    titulo: string;
-    director: string;
-    año: number;
-};
+// Substracts a number
+function substract(){
+  if (substractBtn !==null && substractBtn !== undefined)
+  turno--
+    pCount.innerHTML = turno.toString().padStart(2, "0");
+}
 
-const peliculaA: Pelicula = {
-    titulo: "Blade Runner",
-    director: "Ridley Scott",
-    año: 1982,
-};
+// Resets to value '0'
+function reset(){
+  if (resetBtn !==null && resetBtn !== undefined)
+  turno = 0;
+  pCount.innerHTML = turno.toString().padStart(2, "0");
+}
 
-const peliculaB: Pelicula = {
-    titulo: "Matrix",
-    director: "Lana Wachowski",
-    año: 1999,
-};
+// Adds a number typed manually within the dialog box
+function fun1() {
+  if (btn1 !==null && btn1 !== undefined)
+  pCount.innerHTML = btn1.value.padStart(2, "0")
+}
 
-console.log (peliculaA);
-console.log (peliculaB);
-
+// 'EventListeners used to call the functions to perform a pre-defined task
+addBtn.addEventListener("click", add)
+substractBtn.addEventListener("click", substract)
+resetBtn.addEventListener("click", reset)
+sendNumber.addEventListener("click", fun1)
