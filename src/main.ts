5f007c518ad4566160c6e10298011e10ae1fb11c
Lucas <lgltrad@gmail.com>
2023-07-06 20:40:30 +0200

    Cambios tras feedback

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,77 @@
-
 // Defines the constants for the task
 const addBtn = document.getElementById("añadir");
 const substractBtn = document.getElementById("restar");
 const resetBtn = document.getElementById("reiniciar");
 const btn1 = document.getElementById("number");
 const sendNumber = document.getElementById("btn1");
-const pCount = document.getElementById("count");
+const count = document.getElementById("count");
 
-let turno = 0;
-
-// Adds a number
-function add(){
-  if (pCount !==null && pCount !== undefined && pCount instanceof HTMLParagraphElement){
-  turno++
-  pCount.innerHTML = `${turno}`.padStart(2, "0");
-  } 
-}
-
-// Substracts a number
-function substract(){
-  if (pCount !==null && pCount !== undefined && pCount instanceof HTMLParagraphElement) {
-  turno--
-    pCount.innerHTML = `${turno}`.padStart(2, "0");
-  }
-}
-
-// Resets to value '0'
-function reset(){
-  if (pCount !==null && pCount !== undefined && pCount instanceof HTMLParagraphElement) {
-  turno = 0;
-  pCount.innerHTML = `${turno}`.padStart(2, "0");
-  }
-}
-
-// Adds a number typed manually within the dialog box
-function fun1() {
-  if (pCount !==null && pCount !== undefined && pCount instanceof HTMLParagraphElement) {
-  pCount.innerHTML = getNumber().padStart(2, "0")
-  }
-}
-
-function getNumber() {
-  if (btn1 !==null && btn1 !== undefined && btn1 instanceof HTMLInputElement) {
-    return btn1.value;
-    }
-    return ""
-}
+let turn = 0;
 
 // 'EventListeners used to call the functions to perform a pre-defined task
-if (addBtn !==null && addBtn !== undefined && addBtn instanceof HTMLButtonElement){
+if (addBtn && addBtn instanceof HTMLButtonElement){
   addBtn.addEventListener("click", add);
-}
+};
 
-if (substractBtn !==null && substractBtn !== undefined && substractBtn instanceof HTMLButtonElement) {
+if (substractBtn && substractBtn instanceof HTMLButtonElement) {
   substractBtn.addEventListener("click", substract)
-}
+};
 
-if (resetBtn !==null && resetBtn !== undefined && resetBtn instanceof HTMLButtonElement) {
+if (resetBtn && resetBtn instanceof HTMLButtonElement) {
   resetBtn.addEventListener("click", reset)
-}
+};
 
-if (sendNumber !==null && sendNumber !== undefined && sendNumber instanceof HTMLButtonElement) {
-  sendNumber.addEventListener("click", fun1)
-}
+if (sendNumber && sendNumber instanceof HTMLButtonElement) {
+  sendNumber.addEventListener("click", addNumberManually)
+};
+
+// Adds a number
+function add(): void{
+  if (count && count instanceof HTMLParagraphElement){
+    turn++
+    count.innerHTML = `${turn}`.padStart(2, "0");
+  }
+};
+
+// Substracts a number
+function substract(): void{
+  if (count && count instanceof HTMLParagraphElement) {
+  turn--
+    count.innerHTML = `${turn}`.padStart(2, "0");
+  }
+};
+
+// Resets to value '0'
+function reset(): void{
+  if (count && count instanceof HTMLParagraphElement) {
+  turn = 0;
+  count.innerHTML = `${turn}`.padStart(2, "0");
+  }
+  resetInput();
+};
+
+// Adds a number typed manually within the dialog box
+function addNumberManually(): void {
+  if (count && count instanceof HTMLParagraphElement) {
+  count.innerHTML = getNumber().padStart(2, "0")
+  mapInput();
+  }
+};
+
+function mapInput() {
+  if (btn1 && btn1 instanceof HTMLInputElement)
+  turn = parseInt(btn1.value);
+};
+
+function getNumber(): string {
+  if (btn1 && btn1 instanceof HTMLInputElement) {
+    return btn1.value;
+    }
+    return `${turn}`
+};
+
+function resetInput() {
+  if (btn1 && btn1 instanceof HTMLInputElement){
+    btn1.value = ``;
+  }
+};
